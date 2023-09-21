6cf1c8a55d0f77191cbb12adac77d4d710b14a42
ProgSaleh <progsaleh@hotmail.com>
2023-09-21 05:10:05 +0300

    remove wrong column from model

--- CodePulse.API/Models/Domain/BlogPost.cs
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -5,7 +5,6 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string ShortDescription { get; set; }
-        public string Description { get; set; }
         public string Content { get; set; }
         public string FeaturedImageUrl { get; set; }
         public string UrlHandle { get; set; }
